31b3d145b7b32d2c805ed5bfafb93d32ebbcf0b2
Christine Tran <christinetran825@gmail.com>
2017-11-06 05:45:20 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,24 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  end
+
+end
+
+  describe "POST create" do
+    context "with valid attributes" do
+      it "creates a new student" do
+        expect{
+          post :create, { :first_name => "Sam", :last_name => "Smith" }
+        }.to change(Student,:count).by(1)
+      end
+
+      it "redirects to the new student" do
+        post :create, { :first_name => "Sam", :last_name => "Smith" }
+        expect(response).to redirect_to Student.last
+      end
+    end
   end
 
 end
